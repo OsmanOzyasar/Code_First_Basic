414d1e568f179e3d3918601d69f6b8a3728e3834
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-03-08 00:34:31 +0300

    Add project files.

---
+++ Context/PatikaFirstDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Code_First_Basic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Code_First_Basic.Context
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<MovieEntity> Movies => Set<MovieEntity>();
+        public DbSet<GameEntity> Games => Set<GameEntity>();
+    }
+}
---
+++ Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Code_First_Basic.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
